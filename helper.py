218433ec6167e2e5ae4f2f7ab56606a432218115
Rayhanul Islam <r97yhan@gmail.com>
2023-06-04 00:19:37 -0500

    result showing using plot

--- helper.py
+++ helper.py
@@ -1,12 +1,17 @@
 import random
-
+import math 
 
 
 
 def generate_pqr():
-        r = random.uniform(0, 0.1)
-        p = random.uniform(0, 1-r)
-        q = 1 - p - r
+        r = random.uniform(0, 1)
+        p = random.uniform(0, 1)
+        q= random.uniform(0, 1)
+        factor=p+q+r 
+        p= p/factor
+        q=q/factor
+        r =1-(p+q)
+
         return p, q, r
     
 def generate_pq():
