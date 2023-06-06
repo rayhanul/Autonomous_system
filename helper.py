8fb2b835d4d84d897e2c832c5f26110340275317
Rayhanul Islam <r97yhan@gmail.com>
2023-06-06 18:10:39 -0500

    final model based on control of probabilistic paper model

--- helper.py
+++ helper.py
@@ -3,10 +3,28 @@ import math
 
 
 
+# def generate_pqr():
+#         r = random.uniform(0, .2)
+#         p = random.uniform(0.7, 1)
+#         q= random.uniform(0.5, .7)
+#         factor=p+q+r 
+#         p= p/factor
+#         q=q/factor
+#         r =1-(p+q)
+
+#         return p, q, r
+    
+# def generate_pq():
+#         p = random.uniform(.7, 1)
+#         q = 1 - p
+
+#         return p, q
+
+
 def generate_pqr():
-        r = random.uniform(0, .2)
-        p = random.uniform(0.7, 1)
-        q= random.uniform(0.5, .7)
+        r = random.uniform(0, 1)
+        p = random.uniform(0, 1)
+        q= random.uniform(0, 1)
         factor=p+q+r 
         p= p/factor
         q=q/factor
@@ -15,7 +33,7 @@ def generate_pqr():
         return p, q, r
     
 def generate_pq():
-        p = random.uniform(.7, 1)
+        p = random.uniform(0, 1)
         q = 1 - p
 
         return p, q
--- system_analyzer.py
+++ system_analyzer.py
@@ -19,7 +19,7 @@ class Analyzer:
         self.composed_system=[]
         self.model_path= os.path.join("/home/rayhanul/Documents/GitHub/Autonomous_system", "my_models")
 
-    def create_composed_model(self):
+    def create_composed_model(self):        
         autonomous_car_path = os.path.join(self.model_path, "autonomous_car.nm")
         human_car_path = os.path.join(self.model_path, "human_car.nm")
         env_path= os.path.join(self.model_path, "env_model.nm")
@@ -33,8 +33,8 @@ class Analyzer:
 
         self.composed_system=composed 
         
-    def get_composed_model(self):
-        composed_model_path = os.path.join(self.model_path, "final_combined_model.nm")
+    def get_composed_model(self, final_model_file="final_combined_model.nm"):
+        composed_model_path = os.path.join(self.model_path, final_model_file)
         composed_model = stormpy_int.to_tulip_transys(composed_model_path)
         composed = synchronous_parallel([composed_model])
         return composed 
@@ -44,11 +44,11 @@ class Analyzer:
         with open(path, "w") as file:
             file.write(program)
 
-    def create_combined_model(self, addinational_text):
-        path=os.path.join(self.model_path, 'template_model.txt')
+    def create_combined_model(self, addinational_text, template_file_name, combined_model_name):
+        path=os.path.join(self.model_path, template_file_name)
         with open(path, 'r') as source_file:
             content = source_file.read()
-        path=os.path.join(self.model_path, 'final_combined_model.nm')
+        path=os.path.join(self.model_path, combined_model_name)
         with open(path, 'w') as new_model:
             new_model.write(content)
             new_model.write('\n') 
@@ -81,25 +81,27 @@ class Analyzer:
             return True 
         return False 
         
-def get_policies(analyzer, model, result):
+def get_policies(analyzer, model, result, actions):
     Agent1_pol = dict()
     Agent2_pol = dict()
 
     move_ids = [m_s.id for m_s in model.states for s_i in m_s.labels if 'go' in s_i or 'stop' in s_i]
 
     action_points = set(range(model.nr_states)) - set(move_ids)
-    actions1 = ['go1','stop1']
+    actions1 = actions
     actions2 = ['go2','stop2']
 
     for s_i in action_points:
         # print(model.states[s_i].labels)
+
         for l_i in model.states[s_i].labels:
-            if 'a' in l_i and not 'crash' in l_i:
+            if 'a' in l_i:
                 s_state = l_i
             elif 'h' in l_i:
                 x_state = l_i
             elif 'p' in l_i:
                 p_state = l_i
+
         deterministic_choice=result.scheduler.get_choice(s_i).get_deterministic_choice()
         transition_at_s_i = model.states[s_i].actions[deterministic_choice].transitions
         hold_state = model.states[int(re.findall('\\d+',str(transition_at_s_i))[0])]
@@ -112,9 +114,42 @@ def get_policies(analyzer, model, result):
         # print(text)
         if not analyzer.is_crush_exist(next_state.labels) and 'goal' not in next_state.labels:
             act_tup = tuple()
-            act_tup += ([l_ind for l_ind,l_a in enumerate(actions1) if l_a in next_state.labels][0],)
-            act_tup += ([l_ind for l_ind,l_a in enumerate(actions2) if l_a in next_state.labels][0],)
-            Agent1_pol.update({(s_state, x_state, p_state): act_tup[1] })
+            act_tuple1=[l_ind for l_ind,l_a in enumerate(actions1) if l_a in next_state.labels]
+            # act_tuple2=[l_ind for l_ind,l_a in enumerate(actions2) if l_a in next_state.labels]
+            if len(act_tuple1)>0 :
+                act_tup += (act_tuple1[0],)
+                # act_tup += (act_tuple2[0],)
+                Agent1_pol.update({(s_state, x_state, p_state): act_tup[0] })
+
+        # hold_state2 = model.states[int(re.findall('\\d+', str(model.states[s_i].actions[result.scheduler.get_choice(s_i).get_deterministic_choice()].transitions))[0])]
+        # next_action2 = result.scheduler.get_choice(hold_state2).get_deterministic_choice()
+        # next_state2 = model.states[int(re.findall('\\d+', str(hold_state2.actions[int(next_action2)].transitions))[0])]
+        # if not analyzer.is_crush_exist(next_state2.labels) and 'goal' not in next_state2.labels:
+        #     act_tup2 = tuple()
+        #     act_tup2 += ([l_ind for l_ind,l_a in enumerate(actions1) if l_a in next_state2.labels][0],)
+        #     act_tup2 += ([l_ind for l_ind,l_a in enumerate(actions2) if l_a in next_state2.labels][0],)
+        #     Agent2_pol.update({(s_state,x_state,p_state):act_tup2[0]})
+    return Agent1_pol
+
+def get_policies_for_human(analyzer, model, result):
+
+    Agent2_pol = dict()
+
+    move_ids = [m_s.id for m_s in model.states for s_i in m_s.labels if 'go' in s_i or 'stop' in s_i]
+
+    action_points = set(range(model.nr_states)) - set(move_ids)
+    actions1 = ['go1','stop1']
+    actions2 = ['go2','stop2']
+
+    for s_i in action_points:
+    # print(model.states[s_i].labels)
+        for l_i in model.states[s_i].labels:
+            if 'a' in l_i and not 'crash' in l_i:
+                s_state = l_i
+            elif 'h' in l_i:
+                x_state = l_i
+            elif 'p' in l_i:
+                p_state = l_i
 
 
         hold_state2 = model.states[int(re.findall('\\d+', str(model.states[s_i].actions[result.scheduler.get_choice(s_i).get_deterministic_choice()].transitions))[0])]
@@ -125,31 +160,19 @@ def get_policies(analyzer, model, result):
             act_tup2 += ([l_ind for l_ind,l_a in enumerate(actions1) if l_a in next_state2.labels][0],)
             act_tup2 += ([l_ind for l_ind,l_a in enumerate(actions2) if l_a in next_state2.labels][0],)
             Agent2_pol.update({(s_state,x_state,p_state):act_tup2[0]})
-    return Agent1_pol, Agent2_pol
+    return Agent2_pol
+
 
 def create_dtmc_model_using_policies(analyzer, Agent1_pol, Agent2_pol):
     tc_dtmc = "two_car_dtmc.prism"
     tc_dtmc = os.path.join(analyzer.model_path, tc_dtmc)
-    template= os.path.join(analyzer.model_path, "template_dtmc.txt")
+    template= os.path.join(analyzer.model_path, "template_dtmc_2.txt")
     with open(template) as f:
         with open(tc_dtmc, "w+") as f1:
             for line in f:
                 f1.write(line)
 
-    # dtmc_file = open(tc_dtmc,"a+")
-
     with open(tc_dtmc, 'a+') as dtmc_file:
-        # dtmc_file.write("module car1policy\n\tc1_go1 : bool init false;\n\tc1_stop1 : bool init false;\n\n\t[go] c1_go1 -> 1:(c1_go1'=false);\n\t[stop] c1_stop1 -> 1:(c1_stop1'=false);\n")
-        # pol1_lead = "\t[assign] !carpol1 &"
-        # out1_ind = ["(c1_go1'=true);\n","(c1_stop1'=true);\n"]
-        # for s_z,x,p in Agent2_pol:
-        #     s_num = int(s_z[1:])
-        #     x_num = int(x[1:])
-        #     p_num = int(p[1:])
-        #     state_in = "(a={}) & (h={}) & (p={}) -> ".format(s_num,x_num,p_num)
-        #     pol_ind = 0 if Agent1_pol[(s_z,x,p)] == int(0) else 1
-        #     dtmc_file.write(pol1_lead+state_in+out1_ind[pol_ind])
-        # dtmc_file.write('endmodule\n')
         dtmc_file.write("module car1policy\n\tc1_go1 : bool init false;\n\tc1_stop1 : bool init false;\n\n\t[go] c1_go1 -> 1:(c1_go1'=false);\n\t[stop] c1_stop1 -> 1:(c1_stop1'=false);\n")
         pol1_lead = "\t[assign] !carpol1 &"
         out1_ind = ["(c1_go1'=true);\n","(c1_stop1'=true);\n"]
@@ -165,20 +188,20 @@ def create_dtmc_model_using_policies(analyzer, Agent1_pol, Agent2_pol):
 
         dtmc_file.write('endmodule\n')
 
-                # dtmc_file.write("module car2policy\n\tc2_go2 : bool init false;\n\tc2_stop2 : bool init false;\n\n\t[go2] c2_go2 -> 1:(c2_go2'=false);\n\t[stop2] c2_stop2 -> 1:(c2_stop2'=false);\n")
-        # pol2_lead = "\t[assign] !carpol2 &"
-        # out2_ind = ["(c2_go2'=true);\n", "(c2_stop2'=true);\n"]
+        dtmc_file.write("module car2policy\n\tc2_go2 : bool init false;\n\tc2_stop2 : bool init false;\n\n\t[go2] c2_go2 -> 1:(c2_go2'=false);\n\t[stop2] c2_stop2 -> 1:(c2_stop2'=false);\n")
+        pol2_lead = "\t[assign] !carpol2 &"
+        out2_ind = ["(c2_go2'=true);\n", "(c2_stop2'=true);\n"]
 
-        # for s_z, x, p in Agent2_pol:
-        #     s_num = int(s_z[1:])
-        #     x_num = int(x[1:])
-        #     p_num = int(p[1:])
+        for s_z, x, p in Agent2_pol:
+            s_num = int(s_z[1:])
+            x_num = int(x[1:])
+            p_num = int(p[1:])
 
-        #     state_in = "(a={}) & (h={}) & (p={}) -> ".format(s_num, x_num, p_num)
-        #     pol_ind = 0 if Agent2_pol[(s_z, x, p)] == int(0) else 1
-        #     dtmc_file.write(pol2_lead + state_in + out2_ind[pol_ind])
+            state_in = "(a={}) & (h={}) & (p={}) -> ".format(s_num, x_num, p_num)
+            pol_ind = 0 if Agent2_pol[(s_z, x, p)] == int(0) else 1
+            dtmc_file.write(pol2_lead + state_in + out2_ind[pol_ind])
 
-        # dtmc_file.write('endmodule\n')
+        dtmc_file.write('endmodule\n')
 
 
 
@@ -189,9 +212,11 @@ if __name__=="__main__":
     data_out = dict()
     
     for iter in range(0,numiter):
-        p, q, r = generate_pqr()
         
 
+        # model for Autonomous agent...  
+        p, q, r = generate_pqr()
+        
         mc_2_transition={
             "p2":{"p3":p, "p2":q, "p4":r},
             "p3": {"p8":1}, 
@@ -241,41 +266,84 @@ if __name__=="__main__":
         environment_prism_model=prism_model_generator.get_prism_model()
         
 
-
-
         analyzer=Analyzer()
 
         analyzer.writeToFile(environment_prism_model, "env_model.nm")
         # analyzer.create_composed_model(environment_prism_model)
-        analyzer.create_combined_model(environment_prism_model)
+        analyzer.create_combined_model(environment_prism_model, 'template_model_2.txt', 'final_combined_model.nm')
 
 
         # print("composed done") 
         # property_tobe_verfied='Pmax=?[!(("a3" & "p3") | ("a5" & "p5") | ("a7" & "p7") | ("a3" & "h3") | ("a5" & "h5") | ("a7" & "h7")) U "goal"]'
-        property_tobe_verfied='Pmax=?[(!(("a3" & "p3") | ("a5" & "p5") | ("a7" & "p7"))) U "goal"]'
+        property_tobe_verfied='Pmax=?[(!(("a3" & "p3") | ("a5" & "p5") | ("a7" & "p7")| ("a3" & "h3") | ("a5" & "h5") | ("a7" & "h7"))) U "goal"]'
+        # property_tobe_verfied='Pmax=?[(!(("a3" & "p3") | ("a5" & "p5") | ("a7" & "p7"))) U "goal"]'
         path=os.path.join(analyzer.model_path, 'final_combined_model.nm')
 
         prism_program=stormpy.parse_prism_program(path)
         properties=stormpy.parse_properties(property_tobe_verfied, prism_program)
         model=stormpy.build_sparse_model(prism_program)
 
-        result=stormpy.model_checking(model, properties[0], only_initial_states=False, extract_scheduler=True) 
+        result=stormpy.model_checking(model, properties[0],  extract_scheduler=True) 
 
-        # result ...
+        # result ... 
         initial_state=model.initial_states[0]
 
         belief_model_prob=result.at(initial_state)
 
-        # print("result at initial state: {0}".format(result.at(initial_state)))
+
+
+        # model for human agent 
+        formula_human='Pmax=?[(!(("h3" & "p3") | ("h5" & "p5") | ("h7" & "p7")| ("a3" & "h3") | ("a5" & "h5") | ("a7" & "h7"))) U "goal2"]'
+        path_human=os.path.join(analyzer.model_path, 'final_combined_model.nm')
+
+        prism_program_human=stormpy.parse_prism_program(path_human)
+        properties_human=stormpy.parse_properties(formula_human, prism_program_human)
+        model_human=stormpy.build_sparse_model(prism_program_human)
+
+        result_human=stormpy.model_checking(model_human, properties_human[0],  extract_scheduler=True) 
+
+        # result ... 
+        initial_state_human=model.initial_states[0]
+
+        belief_model_prob_human=result.at(initial_state)
+        #end of human model
+
+
+
+        # scheduler=result.scheduler 
+
+        # dtmc=model.apply_scheduler(scheduler)
+        # print(dtmc)
+
+        # for state in dtmc.states:
+        #     for action in state.actions:
+        #         for transition in action.transitions:
+        #             next_state=dtmc.states[transition.column]
+        #             print(f'state: {state}, label:{state.labels}, action: {action}, next state {transition.column}, label:{next_state.labels}, probability: {transition.value()}')
+        # # print("result at initial state: {0}".format(result.at(initial_state)))
 
         # print(result.scheduler.get_choice(0).get_deterministic_choice())
 
-        Agent1_pol, Agent2_pol = get_policies(analyzer, model, result)
+        Agent1_pol = get_policies(analyzer, model, result, ['go1','stop1'])
 
+        Agent2_pol = get_policies(analyzer, model_human, result_human, ['go2','stop2'])
         
-        create_dtmc_model_using_policies(analyzer, Agent1_pol, Agent2_pol)
+        #policies for human operated vehicle...
+        # path=os.path.join(analyzer.model_path, 'complete_model_human.prism')
+        # prism_program_h=stormpy.parse_prism_program(path)
+        # properties_h=stormpy.parse_properties(property_tobe_verfied, prism_program_h)
+        # model_human=stormpy.build_sparse_model(prism_program_h)
 
+        # result_human=stormpy.model_checking(model_human, properties_h[0], only_initial_states=False, extract_scheduler=True) 
 
+        # print(result.at(model.initial_states[0]))
+        # Agent2_pol= get_policies_for_human(analyzer, model_human, result_human)
+        
+
+        # create complete model applying policies ... 
+        create_dtmc_model_using_policies(analyzer, Agent1_pol, Agent1_pol)
+
+        formula='Pmax=?[(!(("a3" & "p3") | ("a5" & "p5") | ("a7" & "p7")| ("a3" & "h3") | ("a5" & "h5") | ("a7" & "h7")|("h3" & "p3") | ("h5" & "p5") | ("h7" & "p7")| ("a3" & "h3") | ("a5" & "h5") | ("a7" & "h7"))) U "goal"]'
         dtmc_model_path=os.path.join(analyzer.model_path, 'two_car_dtmc.prism')
         prism_program=stormpy.parse_prism_program(dtmc_model_path)
         properties=stormpy.parse_properties(property_tobe_verfied, prism_program)
